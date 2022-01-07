492fee687aedba5eecaa745c231badeab2d6665d
Marcin Kowalski <01169577@pw.edu.pl>
2022-01-07 19:23:09 +0100

    plik naglowkowy do modularnej wersji

--- fun_stack.h
+++ fun_stack.h
@@ -7,9 +7,9 @@ typedef struct stack{
     struct stack *next;
 } *funstack;
 
-int top_of_funstack( void );  // zwraca par_level - "zagłębienie nawiasowe" przechowywane na szczycie
-void put_on_fun_stack( int par_level, char *funame ); // odkłada na stos parę (funame,par_level)
-char *get_from_fun_stack( void ); // usuwa z wierzchołka parę (funame,par_level), zwraca zdjętą funame
+int top_of_funstack( funstack top );  // zwraca par_level - "zagłębienie nawiasowe" przechowywane na szczycie
+void put_on_fun_stack(funstack top, int par_level, char *funame ); // odkłada na stos parę (funame,par_level)
+char *get_from_fun_stack( funstack top ); // usuwa z wierzchołka parę (funame,par_level), zwraca zdjętą funame
 
 #endif
 
