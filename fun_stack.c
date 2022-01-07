7a054667bb6d7a25311d1f68c299ab72b4552319
Marcin Kowalski <01169577@pw.edu.pl>
2022-01-07 14:22:55 +0100

    pierwsza wersja stosu

---
+++ fun_stack.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "fun_stack.h"
+
+int top_of_funstack( void ){
+    if(top == NULL){
+        printf("Stos jest pusty!");
+        return 1;
+    }
+    else{
+        return top->par_level;
+    }
+}
+
+void put_on_fun_stack( int par_level, char *funame ){
+    if(top == NULL){
+        top = malloc(sizeof(top));
+        top->par_level = par_level;
+        top->funame = funame;
+        top->next = NULL;
+    }
+    else{
+        funstack newElem;
+        newElem = malloc(sizeof(newElem));
+        newElem->par_level = par_level;
+        newElem->funame = funame;
+        newElem->next = top;
+        top = newElem;
+    }
+}
+
+char *get_from_fun_stack( void ){
+    if(top == NULL){
+        printf("Stos jest pusty!");
+        return "blad";
+    }
+    else{
+        char *tmp = top->funame;
+        funstack tmp2;
+        tmp2 = top->next;
+        free(top);
+        top = tmp2;
+        return tmp;
+    }
+}
+
---
+++ fun_stack.h
@@ -0,0 +1,15 @@
+#ifndef _FUN_STACK_H_IS_INCLUDED_
+#define _FUN_STACK_H_IS_INCLUDED_
+
+typedef struct stack{
+    int par_level;
+    char *funame;
+    struct stack *next;
+} *funstack;
+
+int top_of_funstack( void );  // zwraca par_level - "zagłębienie nawiasowe" przechowywane na szczycie
+void put_on_fun_stack( int par_level, char *funame ); // odkłada na stos parę (funame,par_level)
+char *get_from_fun_stack( void ); // usuwa z wierzchołka parę (funame,par_level), zwraca zdjętą funame
+
+#endif
+
