49af72602fa7bcc9ec69829fcccffab9b5774215
Marcin Kowalski <01169577@pw.edu.pl>
2022-01-09 17:59:54 +0100

    poprawa dzialania miedzy plikami

--- fun_stack.h
+++ fun_stack.h
@@ -7,6 +7,8 @@ typedef struct stack{
     struct stack *next;
 } *funstack;
 
+extern funstack top;
+
 int top_of_funstack( void );  // zwraca par_level - "zagłębienie nawiasowe" przechowywane na szczycie
 void put_on_fun_stack( int par_level, char *funame ); // odkłada na stos parę (funame,par_level)
 char *get_from_fun_stack( void ); // usuwa z wierzchołka parę (funame,par_level), zwraca zdjętą funame
