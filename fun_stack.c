821b07b3216172c32edcbeea6126428625e3ac25
Marcin Kowalski <01169577@pw.edu.pl>
2022-01-09 17:59:46 +0100

    poprawa dzialania miedzy plikami

--- fun_stack.c
+++ fun_stack.c
@@ -6,7 +6,6 @@ funstack top;
 
 int top_of_funstack( void ){
     if(top == NULL){
-        printf("Stos jest pusty!");
         return 1;
     }
     else{
@@ -16,12 +15,16 @@ int top_of_funstack( void ){
 
 void put_on_fun_stack( int par_level, char *funame ){
     if(top == NULL){
+        printf("%d\n",top);
         top = malloc(sizeof(top));
         top->par_level = par_level;
         top->funame = funame;
         top->next = NULL;
+        printf("%d\n",top);
+        printf("%s\n",top->funame);
     }
     else{
+        printf("okazuje się że stos nie jest pusty");
         funstack newElem;
         newElem = malloc(sizeof(newElem));
         newElem->par_level = par_level;
@@ -42,6 +45,7 @@ char *get_from_fun_stack( void ){
         tmp2 = top->next;
         free(top);
         top = tmp2;
+        printf("to funkcja z gory stosu %s\n",tmp);
         return tmp;
     }
 }
