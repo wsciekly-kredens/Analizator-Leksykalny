7464c7ebd419f19b702d1505cb068949c220eb7e
piotrek246 <96173138+piotrek246@users.noreply.github.com>
2022-01-09 21:03:47 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -2,25 +2,34 @@
 #include <stdlib.h> // exit - ale exit trzeba kiedyś usunąć i nie będzie to potrzebne
 #include "alex.h"       // analizator leksykalny                                     
 #include "fun_stack.h" // stos funkcji
+#include "store.h"
+#include "analizatorSkladni.h"
 
 int main(int argc, char **argv){
-int i=1;
-if(*argv[1]==NULL)
+int k=1;
+if(argv[1]==NULL)
   fprintf(stderr, "Blad - nie moge czytac pliku!\n");
-while(*argv[i]!=NULL)
- {analizatorSkladni (*argv[i]);
-  ++i;}
+while(argv[k]!=NULL)
+ {analizatorSkladni (argv[k]);
+  ++k;}
 
+funkcje=malloc(sizeof(store));
 
-printf("Funkcja:\n");
+int j=0;
+while(funkcje[j]!=NULL)
+{
+
+printf("Funkcja:%s\n", funkcje[j]->fun);
 printf("\tPrototyp:\n");
-printf("\t\tplik od linia do linia\n");
+printf("\t\tplik %s od linia %d do linia %d\n", funkcje[j]->ipname, funkcje[j]->ln, funkcje[j]->ln);
 printf("\tDefinicja:\n");
-printf("\t\tplik od linia do linia\n");
+printf("\t\tplik %s od linia %d do linia %d\n", funkcje[j]->ipname, funkcje[j]->ln, funkcje[j]->ln);
 printf("\tUzycie:\n");
-printf("\t\tplik od linia do linia\n");
+printf("\t\tplik %s od linia %d do linia %d\n", funkcje[j]->ipname, funkcje[j]->ln, funkcje[j]->ln);
 printf("\tWywoluje:\n");
-printf("\t\tplik od linia do linia\n");
+printf("\t\tplik %s od linia %d do linia %d\n", funkcje[j]->ipname, funkcje[j]->ln, funkcje[j]->ln);
 
+++j;
+}
 return 0;
 }
